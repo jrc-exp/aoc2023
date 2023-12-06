89fa3f7c61cd2b5b0ffbfb32b5ed0323719f2ada
Ryan Crawford <joseph.r.crawford@gmail.com>
2023-12-06 05:21:32 -0500

    Day 5

---
+++ aoc/y2023/day5.py
@@ -0,0 +1,191 @@
+""" Day 0 Solutions """
+
+import sys
+from argparse import ArgumentParser
+from collections import Counter, defaultdict
+from itertools import permutations, product
+
+import numpy as np
+
+from aoc.y2023.utils import load_data
+
+
+def ints(x):
+    return list(map(int, x))
+
+
+"""
+The almanac (your puzzle input) lists all of the seeds that need to be planted. It also lists what type of soil to use with each kind of seed, what type of fertilizer to use with each kind of soil, what type of water to use with each kind of fertilizer, and so on. Every type of seed, soil, fertilizer and so on is identified with a number, but numbers are reused by each category - that is, soil 123 and fertilizer 123 aren't necessarily related to each other.
+
+For example:
+
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+The almanac starts by listing which seeds need to be planted: seeds 79, 14, 55, and 13.
+
+The rest of the almanac contains a list of maps which describe how to convert numbers from a source category into numbers in a destination category. That is, the section that starts with seed-to-soil map: describes how to convert a seed number (the source) to a soil number (the destination). This lets the gardener and his team know which soil to use with which seeds, which water to use with which fertilizer, and so on.
+
+Rather than list every source number and its corresponding destination number one by one, the maps describe entire ranges of numbers that can be converted. Each line within a map contains three numbers: the destination range start, the source range start, and the range length.
+
+Consider again the example seed-to-soil map:
+
+50 98 2
+52 50 48
+The first line has a destination range start of 50, a source range start of 98, and a range length of 2. This line means that the source range starts at 98 and contains two values: 98 and 99. The destination range is the same length, but it starts at 50, so its two values are 50 and 51. With this information, you know that seed number 98 corresponds to soil number 50 and that seed number 99 corresponds to soil number 51.
+
+The second line means that the source range starts at 50 and contains 48 values: 50, 51, ..., 96, 97. This corresponds to a destination range starting at 52 and also containing 48 values: 52, 53, ..., 98, 99. So, seed number 53 corresponds to soil number 55.
+
+Any source numbers that aren't mapped correspond to the same destination number. So, seed number 10 corresponds to soil number 10.
+
+So, the entire list of seed numbers and their corresponding soil numbers looks like this:
+
+seed  soil
+0     0
+1     1
+...   ...
+48    48
+49    49
+50    52
+51    53
+...   ...
+96    98
+97    99
+98    50
+99    51
+With this map, you can look up the soil number required for each initial seed number:
+
+Seed number 79 corresponds to soil number 81.
+Seed number 14 corresponds to soil number 14.
+Seed number 55 corresponds to soil number 57.
+Seed number 13 corresponds to soil number 13.
+The gardener and his team want to get started as soon as possible, so they'd like to know the closest location that needs a seed. Using these maps, find the lowest location number that corresponds to any of the initial seeds. To do this, you'll need to convert each seed number through other categories until you can find its corresponding location number. In this example, the corresponding types are:
+
+Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
+Seed 14, soil 14, fertilizer 53, water 49, light 42, temperature 42, humidity 43, location 43.
+Seed 55, soil 57, fertilizer 57, water 53, light 46, temperature 82, humidity 82, location 86.
+Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35, location 35.
+So, the lowest location number in this example is 35.
+
+What is the lowest location number that corresponds to any of the initial seed numbers?
+"""
+
+
+maps = defaultdict(list)
+
+
+def solve(d):
+    """actual solution with puzzle input"""
+    result_1, result_2 = 0, 0
+    print("INPUT DATA:")
+    print(d)
+    seeds = ints(d[0].split(":")[1].strip(" ").split(" "))
+    print(seeds)
+    for row in d[1:]:
+        from string import ascii_lowercase as alphabet
+
+        if not row:
+            continue
+
+        if row[0] in alphabet:
+            source, destination = row.split(" ")[0].split("-")[::2]
+            continue
+        if row[0] in "0123456789":
+            destination_range, source_range, length = ints(row.split(" "))
+            maps[(source, destination)].append((destination_range, source_range, length))
+
+    # for seed in seeds:
+
+    from tqdm.auto import tqdm
+
+    locations = []
+    for seed in tqdm(seeds):
+        val = seed
+        for key in maps.keys():
+            for destination_range, source_range, length in maps[key]:
+                if source_range <= val < source_range + length:
+                    val = destination_range + (val - source_range)
+                    break
+
+        locations.append(val)
+
+    result_1 = min(locations)
+
+    # part 2:
+    seed_starts = seeds[::2]
+    seed_ranges = seeds[1::2]
+    total_seeds = sum(seed_ranges)
+    print(total_seeds)
+    seeds = []
+    result_2 = float("inf")
+    for start, end in tqdm(zip(seed_starts, seed_ranges)):
+        for seed in tqdm(range(start, start + end)):
+            val = seed
+            for key in maps.keys():
+                for destination_range, source_range, length in maps[key]:
+                    if source_range <= val < source_range + length:
+                        val = destination_range + (val - source_range)
+                        break
+            result_2 = min(result_2, val)
+
+    return result_1, result_2
+
+
+def main():
+    """Main function"""
+    args = ArgumentParser()
+    args.add_argument("--skip", action="store_true")
+    args = args.parse_args()
+    # load data:
+    if not args.skip:
+        print("**** TEST DATA ****")
+        d = load_data("test_day5.txt")
+        test_answer_1 = 35
+        test_answer_2 = 46
+        test_solution_1, test_solution_2 = solve(d)
+        assert test_solution_1 == test_answer_1, f"TEST #1 FAILED: TRUTH={test_answer_1}, YOURS={test_solution_1}"
+        assert test_solution_2 == test_answer_2, f"TEST #2 FAILED: TRUTH={test_answer_2}, YOURS={test_solution_2}"
+        print("**** TESTS PASSED ****")
+        print("Test Answer 1: ", test_answer_1)
+        print("My Test Answer 1: ", test_solution_1)
+        print("Test Answer 2: ", test_answer_2)
+        print("My Test Answer 2: ", test_solution_2)
+    print("**** REAL DATA ****")
+    d = load_data("day5.txt")
+    answer_1, answer_2 = solve(d)
+    print("Answer 1:", answer_1)
+    print("Answer 2:", answer_2)
+
+
+if __name__ == "__main__":
+    main()
